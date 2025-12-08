30af5303cb8b037508151fa3939989cb0ab184be
jeekkaaaa <root@telegraf.proxmox>
2025-12-08 17:46:41 +0000

    fix: correct disk type and add cloud-init

--- master/main.tf
+++ master/main.tf
@@ -40,13 +40,15 @@ resource "proxmox_vm_qemu" "k8s_master" {
     format  = "raw"
   }
 
-  # Cloud-Init диск (ИСПРАВЛЕНО: format = "qcow2")
+  # Cloud-Init диск (ПРАВИЛЬНЫЙ СИНТАКСИС)
   disk {
     slot    = "ide2"
     storage = "big_oleg"
     type    = "cloudinit"
-    format  = "qcow2"  # ← КРИТИЧЕСКИ ВАЖНО!
   }
+
+  # Явное указание storage для Cloud-Init
+  cloudinit_cdrom_storage_pool = "big_oleg"
 
   network {
     id     = 0
@@ -60,27 +62,12 @@ resource "proxmox_vm_qemu" "k8s_master" {
   ipconfig0  = "ip=dhcp"
   nameserver = "8.8.8.8"
   
-  # Включаем гостевой агент
-  agent = 1
+  # Включаем гостевой агент (новый формат)
+  agent = "enabled=1,fstrim_cloned_disks=1"
 
   # Ожидание Cloud-Init
   provisioner "local-exec" {
     command = "echo 'Ожидание завершения Cloud-Init...'; sleep 180"
-  }
-
-  # Простая проверка
-  provisioner "remote-exec" {
-    inline = ["echo 'VM created successfully'"]
-    
-    connection {
-      type        = "ssh"
-      user        = "ubuntu"
-      private_key = file(var.ssh_private_key_path)
-      host        = self.default_ipv4_address
-      timeout     = "10m"
-    }
-    
-    on_failure = continue
   }
 
   timeouts {
@@ -100,22 +87,10 @@ resource "proxmox_vm_qemu" "k8s_master" {
   }
 }
 
-# Output переменные (ДОБАВЬТЕ ЭТО!)
-output "vm_info" {
+output "vm_status" {
   value = "ВМ ${proxmox_vm_qemu.k8s_master.name} создана (VMID: ${proxmox_vm_qemu.k8s_master.vmid})"
 }
 
 output "vm_ip" {
   value = proxmox_vm_qemu.k8s_master.default_ipv4_address
-  description = "IP адрес ВМ через гостевой агент"
-}
-
-output "check_commands" {
-  value = <<-EOT
-    Проверка после создания:
-    1. Проверить Cloud-Init: qm config 4000 | grep ide2
-       (должно быть: ide2: big_oleg:4000/vm-4000-cloudinit.qcow2,media=cdrom)
-    2. Проверить гостевой агент: qm guest cmd 4000 ping
-    3. Проверить IP: qm guest cmd 4000 network-get-interfaces
-  EOT
 }
