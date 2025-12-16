62286bb7b7d82eedcb42194ec63f1d76522a62dd
jeekkaaaa <root@telegraf.home.jeekkaaaa>
2025-12-16 08:57:53 +0000

    FIX: Actually add default value for template_specs

--- template/variables.tf
+++ template/variables.tf
@@ -1,3 +1,4 @@
+# Proxmox API
 variable "pm_api_url" {
   type = string
 }
@@ -10,30 +11,79 @@ variable "pm_api_token_secret" {
   type = string
 }
 
+# Основные настройки
 variable "target_node" {
-  type    = string
-  default = "pve-k8s"
+  type = string
 }
 
+# Шаблон
 variable "template_vmid" {
-  type    = number
-  default = 9000
+  type = number
 }
 
+# SSH ключ
 variable "ssh_public_key" {
   type = string
 }
 
-variable "storage" {
-  type    = string
-  default = "local-lvm"
+# IP база (для совместимости)
+variable "static_ip_base" {
+  type = number
 }
 
-variable "bridge" {
-  type    = string
-  default = "vmbr0"
+# Конфигурация сети
+variable "network_config" {
+  type = object({
+    subnet       = string
+    gateway      = string
+    dns_servers  = list(string)
+    bridge       = string
+  })
 }
 
+# Конфигурация кластера
+variable "cluster_config" {
+  type = object({
+    masters_count = number
+    workers_count = number
+    cluster_name  = string
+    domain        = string
+  })
+}
+
+# VM ID диапазоны
+variable "vmid_ranges" {
+  type = object({
+    masters = object({ start = number, end = number })
+    workers = object({ start = number, end = number })
+  })
+}
+
+# Спецификации VM
+variable "vm_specs" {
+  type = object({
+    master = object({
+      cpu_cores         = number
+      cpu_sockets       = number
+      memory_mb         = number
+      disk_size_gb      = number
+      disk_storage      = string
+      disk_iothread     = bool
+      cloudinit_storage = string
+    })
+    worker = object({
+      cpu_cores         = number
+      cpu_sockets       = number
+      memory_mb         = number
+      disk_size_gb      = number
+      disk_storage      = string
+      disk_iothread     = bool
+      cloudinit_storage = string
+    })
+  })
+}
+
+# Спецификации шаблона - ВАЖНО: default ЗДЕСЬ!
 variable "template_specs" {
   type = object({
     cpu_cores     = number
@@ -42,11 +92,28 @@ variable "template_specs" {
     disk_size_gb  = number
     disk_iothread = bool
   })
+  default = {
+    cpu_cores     = 2
+    cpu_sockets   = 1
+    memory_mb     = 2048
+    disk_size_gb  = 12
+    disk_iothread = true
+  }
 }
 
+# Cloud-init
 variable "cloud_init" {
   type = object({
     user           = string
     search_domains = list(string)
   })
+}
+
+# Общие настройки
+variable "storage" {
+  type = string
+}
+
+variable "bridge" {
+  type = string
 }
